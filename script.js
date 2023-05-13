287a0e48cc270df2d6b447f572564e7f52ddadfd
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-13 09:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const digitToLettersMap = {
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz',
+};
+
 function letterCombinations(input_digit) {
-  //Complete the function
+const result = [];
+  
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const letters = digitToLettersMap[nextDigits[0]];
+      for (let i = 0; i < letters.length; i++) {
+        backtrack(combination + letters[i], nextDigits.slice(1));
+      }
+    }
+  }
+  
+  if (input_digit) {
+    backtrack('', input_digit);
+  }
+  
+  return result;
+	
 }
 
 module.exports = letterCombinations;
